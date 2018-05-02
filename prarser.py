45b13b9ec9ba9e06493ae8cf91d73105800c11a5
Omar Alhammadi <sequencek@gmail.com>
2018-05-02 20:14:17 +0400

    init

---
+++ prarser.py
@@ -0,0 +1,19 @@
+#hello
+
+
+
+def getTests():
+  found=True;
+  i=1;
+  while found:
+    try:
+      f = open(str(i)+".txt");
+    except FileNotFoundError:
+      return;
+    yield f;
+    i+=1;
+
+t = getTests();
+
+for f in t:
+  print(f);
